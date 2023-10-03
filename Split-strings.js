b5a719452c7d87fb8c78afe1f04fbb92abd87f84
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-03 11:09:07 -0700

    Update Split-strings.js

--- Split-strings.js
+++ Split-strings.js
@@ -10,3 +10,16 @@ Examples:
 
 
 // Solution
+
+function solution(str) {
+  let i = 0;
+  let result = new Array();
+  if (str.length % 2 !== 0) {
+    str = str + '_'
+  }
+  while (i < str.length) {
+    result.push(str[i] + str[i+1]);
+    i += 2;
+  }
+  return result;
+}
